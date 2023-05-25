9d61fbe101a24d111ba1077d727c5ac84c16cb86
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-25 10:33:52 -0300

    Tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,26 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
