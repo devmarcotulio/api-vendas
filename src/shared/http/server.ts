eb4e718e4797837a02064c2a5bccf195a830afe1
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-29 15:15:35 -0300

    Validação dos dados ajustados nas rotas

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/appError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
