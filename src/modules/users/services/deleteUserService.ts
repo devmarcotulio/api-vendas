39502681e6b737fd24b8a226bbbf130e56bb2b0c
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-31 09:03:01 -0300

    Delete User Service criado

---
+++ src/modules/users/services/deleteUserService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/usersRepository';
+import AppError from '@shared/errors/appError';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteUserService {
+  public async execute(id: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const users = await usersRepository.findOne(id);
+
+    if (!users) {
+      throw new AppError('User not found');
+    }
+
+    await usersRepository.delete(users);
+  }
+}
+
+export default DeleteUserService;
