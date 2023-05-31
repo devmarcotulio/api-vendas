a0ed3be0b7e9e9377e25d425b9bd686d71096b0f
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-31 09:00:52 -0300

    Show User Service criado

---
+++ src/modules/users/services/showUserService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/user';
+import UsersRepository from '../typeorm/repositories/usersRepository';
+import AppError from '@shared/errors/appError';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowUserService {
+  public async execute(id: IRequest): Promise<User | undefined> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const users = await usersRepository.findOne(id);
+
+    if (!users) {
+      throw new AppError('User not found');
+    }
+
+    return users;
+  }
+}
+
+export default ShowUserService;
