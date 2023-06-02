98e64373eaef56d30bb3527bf0b675823f9c41c3
Marco Tulio <marcotulio_iub@hotmail.com>
2023-06-02 16:41:09 -0300

    Migration UserToken

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,6 +6,7 @@ import routes from './routes';
 import AppError from '@shared/errors/appError';
 import '@shared/typeorm';
 import { errors } from 'celebrate';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
