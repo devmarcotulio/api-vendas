7d106fb7f207d1e972535b523ef56285ec3a8cd4
Marco Tulio <marcotulio_iub@hotmail.com>
2023-06-06 09:34:40 -0300

    Ajuste rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/productRoutes';
 import usersRouter from '@modules/users/routes/userRoutes';
 import sessionsRouter from '@modules/users/routes/sessionsRoutes';
+import passwordRouter from '@modules/users/routes/passwordRoutes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
