f4ae4fd47ee6951c64fab2fd8ddebbf728df8346
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-02 12:24:48 -0300

    Rotas atualizadas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/productRoutes';
 import usersRouter from '@modules/users/routes/userRoutes';
 import sessionsRouter from '@modules/users/routes/sessionsRoutes';
 import passwordRouter from '@modules/users/routes/passwordRoutes';
+import profileRouter from '@modules/users/routes/profileRoutes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
