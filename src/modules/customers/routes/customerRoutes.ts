1fec183ad6b1d6facb48667f8ee949f57a87e336
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-03 17:28:38 -0300

    Customer routes

---
+++ src/modules/customers/routes/customerRoutes.ts
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import CustomersController from '../controllers/customersController';
+
+const customersRouter = Router();
+const customerController = new CustomersController();
+
+customersRouter.get('/', customerController.index);
+
+customersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customerController.show,
+);
+
+customersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+  }),
+  customerController.create,
+);
+
+customersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customerController.update,
+);
+
+customersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customerController.delete,
+);
+
+export default customersRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/users/routes/userRoutes';
 import sessionsRouter from '@modules/users/routes/sessionsRoutes';
 import passwordRouter from '@modules/users/routes/passwordRoutes';
 import profileRouter from '@modules/users/routes/profileRoutes';
+import customersRouter from '@modules/customers/routes/customerRoutes';
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
