8f42d07c0f2710bce39fbf2acca6bc59a6572277
Marco Tulio <marcotulio_iub@hotmail.com>
2023-08-07 14:54:59 -0300

    Orders controller

---
+++ src/modules/orders/controllers/OrdersController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import ShowOrderService from '../services/ShowOrderService';
+import CreateOrderService from '../services/createOrderService';
+
+class OrdersController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { customer_id, products } = req.body;
+
+    const createOrder = new CreateOrderService();
+
+    const order = await createOrder.execute({
+      customer_id,
+      products,
+    });
+
+    return res.json(order);
+  }
+
+  public async show(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const showOrder = new ShowOrderService();
+
+    const order = showOrder.execute({ id });
+
+    return res.json(order);
+  }
+}
+
+export default OrdersController;
